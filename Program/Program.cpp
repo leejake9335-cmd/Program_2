8f46c9342b9021081dc09472002b78980da5ddc9
leejake9335-cmd <leejake9335@gmail.com>
2026-06-12 19:44:59 +0900

    (2026.06.12) 이중 연결 리스트

--- Program/Program.cpp
+++ Program/Program.cpp
@@ -1,85 +1,39 @@
 ﻿#include <iostream>
 
-using namespace std;
-
-template <typename T>
-
+template<typename T>
 class List
 {
-private:
+private :
 	struct Node
 	{
 		T data;
 		Node* next;
+		Node* previous;
 	};
 
 	int size;
 
 	Node* head;
 
+	Node* tail;
+
 public:
+
 	List()
 	{
 		size = 0;
 		head = nullptr;
+		tail = nullptr;
 	}
-	void push_front(T data)
-	{
-		Node* newNode = new Node;
-
-		newNode->data = data;
-
-		if (head == nullptr)
-		{
-			head = newNode;
-			
-			newNode->next = nullptr;
-		}
-		else
-		{
-			newNode->next = head;
-			
-			head = newNode;
-		}
-		size++;
-	}
-	void pop_front()
-	{
-		if (head == nullptr)
-		{
-			cout << "Linked list is empty." << endl;
-		}
-		else
-		{
-			Node* deleteNode = head;
-			
-			head = head->next;
-			
-			delete deleteNode;
-			
-			size--; 
-		}
-	}
-	void push_back(T data)
-
-
+	
+	
 };
 
-int main()
-{
-	List<int> list;
+	int main()
+	{
 
-	list.push_front(10);
+		List<int> list;
+	
+		return 0;
 
-	list.push_front(5);
-
-	list.pop_front();
-
-	list.pop_front();
-
-	list.pop_front();
-
-
-	return 0;
-}
-
+	}
