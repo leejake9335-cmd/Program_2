013d3e4bde94c3b409160e2e09caabf18ddd96ec
leejake9335-cmd <leejake9335@gmail.com>
2026-06-17 19:43:53 +0900

    (2026.06.17) 스택

--- Program/Program.cpp
+++ Program/Program.cpp
@@ -4,102 +4,33 @@ using namespace std;
 
 template <typename T>
 
-class List
+class Vector
 {
 private:
-	struct Node
-	{
-		T data;
-		Node* next;
-	
-	};
+	int m_size;
+	int capacity;
 
-	int size;
-
-	Node* head;
-
+	T * container;
 public:
-	List()
+	Vector()
 	{
-		size = 0;
-		head = nullptr;
+		m_size = 0;
+		capacity = 0;
+		container = nullptr;
 	}
-	void push_front(T data)
+	const int& size() 
 	{
-		Node* newNode = new Node;
+		return m_size;
+	}
+	const & operator
 
-		newNode->data = data;
-		
-		if (head == nullptr)
-		{
-			head = newNode;
-			newNode->next = head;
-		}
-		else
-		{
-			newNode->next = head->next;
-			head ->next = newNode;
-			
-		}
-		size++;
-	}
-
-	void pop_front()
-	{
-		if (head == nullptr)
-		{
-			cout << "Linked list is empty." << endl;
-		}
-		else
-		{
-			Node* deleteNode = head->next;
-			if (head == head->next)
-			{
-				head = nullptr;
-			}
-			else
-			{
-				head->next = deleteNode->next;
-			}
-			delete deleteNode;
-		    size--;
-		}
-	}
-	void push_back(T data)
-	{
-		Node* newNode = new Node;
-		newNode->data = data;
-		newNode->next = nullptr;
-		if (head == nullptr)
-		{
-			head = newNode;
-			newNode->next = head;
-		}
-		else
-		{
-			Node* currentNode = head;
-			while (currentNode->next != head)
-			{
-				currentNode = currentNode->next;
-			}
-			currentNode->next = newNode;
-			newNode->next = head;
-		}
-		size++;
-	}
 };
 
-	int main()
-	{
-		List<int> list;
-		
-		list.push_front(10);
-		list.push_front(5);
 
-		list.pop_front();
-		list.pop_front();
-		list.pop_front();	
+int main()
+{
+	Vector<int> vector;
 
 		return 0;
 
-	}
+}
