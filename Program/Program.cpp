36d6aff509183a5b617c48808c735512ce953301
leejake9335-cmd <leejake9335@gmail.com>
2026-06-21 23:04:34 +0900

    (2026.06.19) 원형 큐

--- Program/Program.cpp
+++ Program/Program.cpp
@@ -3,83 +3,80 @@
 using namespace std;
 
 template <typename T>
-
-class Vector
+class Stack
 {
 private:
-	int m_size;
+	int highset;
 	int capacity;
 
 	T * container;
-public:
-	Vector()
-	{
-		m_size = 0;
-		capacity = 0;
-		container = nullptr;
-	}
-	const int& size()
-	{
-		return m_size;
-	}
 
-	const T& operator [] (const int& index)
+public:
+	Stack()
 	{
-		return container[index];
-	}
-	~Vector()
-	{
-		delete[] container;
+		highset = -1;
+		capacity = 0;
+
+		container = nullptr;
+
 	}
 
 	void resize(int newSize)
 	{
-		// 1. capacity에 새로운 size 값을 저장합니다.
 		capacity = newSize;
 
-		// 2. 새로운 포인터 변수를 생성해서 새롭게 만들어진
-		//    메모리 공간을 가리키도록 합니다.
-		T * temporary = new T[capacity];
-
-		// 3. 새로운 메모리 공간의 값을 초기화 합니다.
+		T* temporary = new T[capacity];
 		for (int i = 0; i < capacity; i++)
-		{
-			temporary[i] = NULL;
-		}
-		
-		// 4. 기존 배열에 있는 값을 복사해서 새로운
-		//    배열에 넣어줍니다.
-		for (int i = 0; i < size(); i++)
 		{
 			temporary[i] = container[i];
 		}
 		
-		// 5, 기본 배열의 메모리를 해제합니다.
-        delete[] container;
-		
-		// 6, 기존 배열을 가리키던 포인터 변수의 값을
-		//    새로운 배열의 시작 주소로 가리킵니다.
-		container = temporary;
+		delete[] container;
 
+		container = temporary;
 	}
-	void push_back(T data)
+
+	void push(T data)
 	{
 		if (capacity <= 0)
-
+		{
+			resize(1);
+		}
+		else if (highset + 1 >= capacity)
+		{
+			resize(capacity * 2);
+		}
+		
+		container[++highset] = data;
 	}
 
+	void pop()
+	{
+		if (highset <= -1)
+		{
+			cout << "stack is empty" << endl;
+		}
+		else
+		{
+			container[highset--] = NULL;
+		}
+	}
 
 };
 
-
-
 int main()
 {
-	Vector<int> vector;
-
-	vector.push_back(10);
-
+	Stack <int> stack;
 	
+	stack.resize(1);
+	stack.resize(2);
+	stack.resize(4);
+
+	stack.pop();
+	stack.pop();
+	stack.pop();
+
+
 	return 0;
 
 }
