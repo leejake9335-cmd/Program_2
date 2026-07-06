76150596bb8ee2e13f5a5a008d5f0df74be9fa79
leejake9335-cmd <leejake9335@gmail.com>
2026-07-06 19:44:11 +0900

    (2026.07.06) 균형 이진 트리

--- Program/Program.cpp
+++ Program/Program.cpp
@@ -1,6 +1,61 @@
 ﻿#include <iostream>
 
 using namespace std;
+template <typename T>
+class Graph
+{
+private:
+	int size;        // 정점의 개수
+	int count;       // 인접 행렬의 크기
+	int capacity;    // 최대 용량
+
+	T* vertex;       // 정점의 집합
+	int** matrix;    // 인접 행렬
+public:
+	Graph()
+	{
+		size = 0;
+		count = 0;
+		capacity = 0;
+
+		vertex = nullptr;
+		matrix = nullptr;
+	}
+	void resize(int newSize)
+	{
+		capacity = newSize;
+
+		T* container = new T[capacity];
+		for (int i = 0; i < capacity; i++)
+		{
+			container[i] = NULL;
+		}
+
+		for (int i = 0; i < size; i++)
+		{
+			container[i] = vertex[i];
+		}
+
+		delete[] vertex;
+
+		vertex = container;
+	}
+
+	void push(T data)
+	{
+		if (capacity <= 0)
+		{
+			resize(1);
+		}
+		else if (size >= capacity)
+		{
+			resize(capacity * 2);
+		}
+		vertex[size++] = data;
+	}
+
+
+};
 
 
 int main()
